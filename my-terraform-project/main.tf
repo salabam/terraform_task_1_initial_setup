2600d5c8ace24faf0d47db3e165d294ca6a0ceb7
salabam <pavelgrom.chaban@gmail.com>
2026-02-13 16:55:51 +0200

    Solution

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = "fuck-you-gp"
+  name     = "terraform-rg-1"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "fuckyoustorage"
+  name                     = "storageaccount1"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
